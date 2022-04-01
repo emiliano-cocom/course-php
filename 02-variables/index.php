85d85968587207dd0c59910eb7f30f57e6e61054
Emiliano Cocom <joseemilianococom@gmail.com>
2022-04-01 00:02:24 -0600

    variables

---
+++ 02-variables/index.php
@@ -0,0 +1,16 @@
+<?php
+    $name = 'José Cocom';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <h1><?php echo 'Bienvenido '.$name; ?></h1>
+</body>
+</html>
