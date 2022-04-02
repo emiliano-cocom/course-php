3708f0b78477b173d519db8a7d99eaa46696bcca
Emiliano Cocom <joseemilianococom@gmail.com>
2022-04-01 22:28:36 -0600

    types data

---
+++ 04-types-data/index.php
@@ -0,0 +1,14 @@
+<?php
+    // number
+    $number = 123;
+    // string
+    $string = 'Jose';
+    // float
+    $float = 123.45;
+    // double
+    $double = 123.585858585949383838383883;
+    // boolean
+    $boolean = true;
+    // escape quotes
+    $escapeQuotes = "Hello, my name \$is: \"José\" and i am 23 years old";
+?>
