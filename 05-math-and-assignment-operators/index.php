723b23581538892a04eec72ee1ecc91824e60bd6
Emiliano Cocom <joseemilianococom@gmail.com>
2022-04-01 22:49:18 -0600

    math and assignment operators

---
+++ 05-math-and-assignment-operators/index.php
@@ -0,0 +1,21 @@
+<?php
+    // assignment =
+    $assignment = 'José';
+    // unario oprator -
+    $variable = 34;
+    $unario = -$variable;
+
+    /* --- math operators */
+    $a = 2;
+    $b = 5;
+    // sum
+    $sum = $a + $b;
+    // remainder
+    $remainder = $a - $b;
+    // numtiplication
+    $multiplication = $a * $b;
+    // division
+    $division = $a / $b;
+    // module
+    $module = $a % $b;
+?>
