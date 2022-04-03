c353e607227115cd74c8579041f22f541d5edc5b
Emiliano Cocom <joseemilianococom@gmail.com>
2022-04-03 12:24:01 -0500

    defined functions

---
+++ 14-functions/index.php
@@ -0,0 +1,7 @@
+<?php
+    // defined functions
+    echo 'timestamp: '.time().'<br>';
+    echo 'square root of 9: '.sqrt(9).'<br>';
+    echo 'random number between 90 and 100: '.rand(90, 100).'<br>';
+    echo 'number pi: '.pi();
+?>
