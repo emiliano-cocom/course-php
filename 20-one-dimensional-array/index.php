ec7a40872b4f733862a1fbca9968d01f1a7a9304
Emiliano Cocom <joseemilianococom@gmail.com>
2022-04-07 01:36:18 -0500

    one dimensional array

---
+++ 20-one-dimensional-array/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $array = [10, 13, 15, 20];
+
+    // add new value to array
+    $array[4] =  17;
+    // add new value to array
+    $array[] = 25;
+    
+    // array with different types data
+    $arrayTwo = [10, 'Jose', 15, 20, false];
+
+    // array with string key
+    $arrayThree = [
+        'name' => 'Jose',
+        'email' => 'jose@gmail.com',
+        'phone' => '99-89-77-33-05',
+    ];
+?>
