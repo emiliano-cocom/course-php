e8df0847f1d7d466591262036adf822214435ba3
Emiliano Cocom <joseemilianococom@gmail.com>
2022-04-16 12:23:47 -0500

    inheritance

--- POO/classes/Person.php
+++ POO/classes/Person.php
@@ -3,16 +3,19 @@
     class Person
     {
         public $name;
-        public $lastName;
+        public $firstLastName;
+        public $secondLastName;
         public $age;
 
         // constructor
+        /*
         public function __construct($name, $lastName, $age)
         {
             $this->name = strtolower($name);
             $this->lastName = strtolower($lastName);
             $this->age = $age;
         }
+        */
 
         // setter
         public function setName($name)
@@ -24,5 +27,47 @@
         public function getName()
         {
            return ucwords($this->name);
+        }
+
+        // setter
+        public function setLastName($firstLastName, $secondLastName)
+        {
+            $this->firstLastName = $firstLastName;
+            $this->secondLastName = $secondLastName;
+        }
+
+        // getter
+        public function getLastName()
+        {
+            return $this->firstLastName." ".$this->secondLastName;
+        }
+    }
+
+
+    // inheritance
+    class Mexican extends Person
+    {
+        public $departament;
+        public $city;
+
+        // modify setter
+        public function setLastName($firstLastName, $secondLastName)
+        {
+            parent::setLastName($firstLastName, $secondLastName);
+            echo "surnames were assigned successfully";
+        }
+    }
+
+    // inheritance
+    class Peruvian extends Person
+    {
+        public $commune;
+        public $region;
+
+        // overwrite setter
+        public function setLastName($firstLastName, $secondLastName)
+        {
+            $this->firstLastName = $secondLastName;
+            $this->secondLastName = $firstLastName;
         }
     }
--- POO/index.php
+++ POO/index.php
@@ -9,10 +9,27 @@
     // $person->age = 22;
 
     $personTwo = new Person('Elizabeth', 'Martinez', 19);
-    // $personTwo->setName('Elizabeth');
+    $personTwo->setName('Elizabeth');
     // $personTwo->lastName = 'Martinez';
     // $personTwo->age = 19;
 
-    echo $person->getName();
-    echo $personTwo->getName();
+    echo $person->getName().'<br>';
+    echo $personTwo->getName().'<br>';
+
+    // inheritance
+
+    $mexican = new Mexican;
+    $mexican->setName('Jose');
+    $mexican->setLastName('Martinez', 'Lopez');
+    echo '<br>';
+
+    $peruvian = new Peruvian;
+    $peruvian->setLastName('Martinez', 'Lopez');
+
+    echo $mexican->getLastName().'<br>';
+    echo $peruvian->getLastName().'<br>';
+
+    var_dump($mexican);
+    echo '<br>';
+    var_dump($peruvian);
 ?>
