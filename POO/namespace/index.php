6bf9f74b924d61e8dd25fe678f5f581008eea9e4
Emiliano Cocom <joseemilianococom@gmail.com>
2022-04-17 08:45:14 -0500

    comment autoload method

--- POO/namespace/index.php
+++ POO/namespace/index.php
@@ -3,6 +3,7 @@
     use Controllers\CourseController;
     use Models\Course;
 
+    // autoload files
     spl_autoload_register(function($class) {
         // echo str_replace('\\', '/', $class).'.php';
         if (file_exists(str_replace('\\', '/', $class).'.php')) {
